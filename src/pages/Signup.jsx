24e605040e42e552ca0f4c3e4cb53ac8e885a7db
Mohammad Zaid <mohdzaid07091997@gmail.com>
2025-07-19 07:26:06 +0530

    Height adjusted in Signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,7 +4,7 @@ import { Signup as SignupComponent } from '../components/index'
 
 function Signup() {
     return (
-        <div className='py-8 h-905vh]'>
+        <div className='py-8 h-95vh]'>
             <SignupComponent />
         </div>
     )
